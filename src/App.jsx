0f478283818e5cd0987dfc83e2b9f4b1b4f66a4e
Feyalon <gafursharipov3@gmail.com>
2023-07-24 22:35:24 +0500

    Added Sorting

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [posts, setposts] = useState([])
-  const pages = 10;
   const pagesArr = [];
-  for(let i = 1; i <= pages; i++){
-    pagesArr.push(i)
-  }
+  const [issortAlphabet, setSortAlphabet] = useState(false)
   const [currpage, setPage] = useState(1)
   function nextPage(){
     if(currpage >= 10){
@@ -25,28 +22,62 @@ function App() {
       setPage(currpage - 1)
     }
   }
+  const titleNames = []
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/?_page=${currpage}&_limit=10`)
     .then(res => res.json())
     .then((data) => {
+      
       setposts(data)
     })
   }, [currpage])
+  for(let i = 0; i < posts.length; i++){
+    
+    titleNames.push(posts[i].title)
+
+  }
+  titleNames.sort()
+  function sorted(){
+    const arr = []
+    for(let i = 0; i < posts.length; i++){
+      arr.push(posts[i].title)
+      
+      
+    }
+    arr.sort()
+    return arr
+  }
+  sorted()
+  for(let i = 1; i <= posts.length; i++){
+    pagesArr.push(i)
+  }
+  function setSorted(){
+    setSortAlphabet(!issortAlphabet)
+  }
   return (
     <div>
       <h1>
         {currpage}
       </h1>
-      {posts.map((name) => (
-        <div key={name.id}>
-          
-          <h1>
-
-            {name.id} {name.title}
-          </h1>
+      <button onClick={() => setSorted()}>
+        Сортировать по алфавиту
+      </button>
+      {
+        posts.map((name) => (
           <p>
-            {name.body}
+            {name.id}
           </p>
+        ))
+      }
+      { issortAlphabet === false ? posts.map((name) => (
+        <div key={name.id}>
+          <h1>{name.title}</h1>
+        </div>
+      )) : titleNames.map((name, i) => (
+        <div key={i}>
+          <h1>
+            {name}
+          </h1>
         </div>
       ))}
       <button onClick={previousPage}>
